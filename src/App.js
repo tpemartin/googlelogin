a76c1d161b3bb1949311606e437f7f3c5916b29b
Martin老師 <tpemartin@gmail.com>
2022-12-30 05:10:40 +0800

    before building component packages

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { SignIn } from './Authentication';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <SignIn/>
+    </div>
+  );
+}
+
+export default App;
